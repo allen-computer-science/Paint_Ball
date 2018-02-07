e9e169782ee2a5adb528c8dbb611c9d176b1a335
Vathys <deepdesai00@gmail.com>
2018-02-07 16:50:57 -0600

    Framework complete

--- src/main/java/org/paintball/engine/Timer.java
+++ src/main/java/org/paintball/engine/Timer.java
@@ -2,5 +2,24 @@ package main.java.org.paintball.engine;
 
 public class Timer
 {
-
+	private double lastLoopTime;
+	
+	public void init() {
+		lastLoopTime = getTime();
+	}
+	
+	public double getTime() {
+		return System.nanoTime() / 1_000_000_000.0;
+	}
+	
+	public float elapsedTime() {
+		double time = getTime();
+		float elapsedTime = (float)(time - lastLoopTime);
+		lastLoopTime = elapsedTime;
+		return elapsedTime;
+	}
+	
+	public double getLastLoopTime() {
+		return lastLoopTime;
+	}
 }
--- src/main/java/org/paintball/game/Main.java
+++ src/main/java/org/paintball/game/Main.java
@@ -1,11 +1,13 @@
 package main.java.org.paintball.game;
 
+import main.java.org.paintball.engine.GameEngine;
+
 public class Main
 {
 
 	public static void main(String[] args)
 	{
-		
+		new GameEngine("Paint_Ball", 1000 / 12 * 9, 1000).start();;
 	}
 
 }
