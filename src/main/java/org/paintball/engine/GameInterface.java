160486469cb8bb66177d6971bcd1a46713ca8be7
Vathys <deepdesai00@gmail.com>
2018-02-07 16:31:24 -0600

    Framework Update

--- src/main/java/org/paintball/engine/GameInterface.java
+++ src/main/java/org/paintball/engine/GameInterface.java
@@ -1,6 +1,27 @@
 package main.java.org.paintball.engine;
 
-public interface GameInterface
-{
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
+@SuppressWarnings("serial")
+public abstract class GameInterface extends Canvas
+{	
+	private BufferStrategy bs;
+	
+	public abstract void init();
+	
+	public abstract void update();
+	
+	public abstract void paint(Graphics g);
+	
+	public Graphics getGraphics() {
+		bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return null;
+		}
+		
+		return bs.getDrawGraphics();
+	}
 }
