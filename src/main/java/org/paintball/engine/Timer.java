ee2c40a1d1046cf4a0f1ecb9a0d6bb5e713378f3
ErikTheCleric <erikhale@outlook.com>
2018-02-26 13:18:56 -0600

    Merge problems

--- src/main/java/org/paintball/engine/Timer.java
+++ src/main/java/org/paintball/engine/Timer.java
@@ -1,6 +1,28 @@
+/* The Timer.java class is the timer set for making the animations on the screen
+ * run smoothly. - erikTheCleric - 2/7/2018 
+ */
 package main.java.org.paintball.engine;
 
 public class Timer
 {
-
+	public double lastLoopTime;
+	
+	public void init() {
+		lastLoopTime = getTime();
+	}
+	
+	public double getTime() {
+		return System.nanoTime() / 1000000000.0; // 1,000,000,000 nanoseconds = 1 second
+	}
+	
+	public float elapsedTime() {
+		double time = getTime();
+		float elapsedTime = (float)(time - lastLoopTime);
+		lastLoopTime = elapsedTime;
+		return elapsedTime;
+	}
+	
+	public double getLastLoopTime() {
+		return lastLoopTime;
+	}
 }
